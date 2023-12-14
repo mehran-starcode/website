96105f048ebfe78e76ebdf72c337f2ba27f03bd3
mehran-starcode <mehran2bb@gmail.com>
2023-12-14 22:10:18 +0330

    sdfds

--- Static/JS/banner.js
+++ Static/JS/banner.js
@@ -6,14 +6,15 @@ import * as content from "./content.js";
 import * as sidebar from "./sidebar.js";
 import * as footer from "./footer.js";
 // ----------------------------------  [ELs] && [STs]  ------------------------------------
-const bannerVideo_EL = document.querySelector('#banner > video')
+const bannerVideo_EL = document.querySelector('#banner > video');
+const headerBanner_EL = document.getElementById("headerBanner");
 // ---------------------------------------- Body  -----------------------------------------
 
 window.addEventListener('load',event=>{
     bannerVideo_EL.play()
 })
 
-
+headerBanner_EL.innerHTML = navigator.platform
 
 
 

9be427f6787393e9a8ec3a861fc6a6a232ae82cd
mehran-starcode <mehran2bb@gmail.com>
2023-12-18 21:31:16 +0330

    create Static files of secondPage

---
+++ mainPage/Static/JS/secondPage/main.js
@@ -0,0 +1,22 @@
+'use strict'
+// -------------------------------------  Imports ----------------------------------------
+import * as navbar from "./navbar.js";
+import * as banner from "./banner.js";
+import * as content from "./content.js";
+import * as sidebar from "./sidebar.js";
+import * as footer from "./footer.js";
+// ----------------------------------  [ELs] && [STs]  ------------------------------------
+
+// ---------------------------------------- Body  -----------------------------------------
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------------  Exports  -----------------------------------------
+export {}

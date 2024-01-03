0ec962d84abc37f97f4f382b6ff6d32b45fb2d62
mehran-starcode <mehran2bb@gmail.com>
2024-01-03 20:00:00 +0330

    sdf

--- Static/JS/mainPage/include/sidebar/sidebar.js
+++ Static/JS/mainPage/include/sidebar/sidebar.js
@@ -3,15 +3,19 @@
 import * as main from "../../../main/main.js";
 import * as Variables from "./sidebar_Variables.js";
 // ---------------------------------------- Body  -----------------------------------------
-Variables.sidebar_EL.addEventListener('click',event=>{
-    Variables.pip_ELs[0].style.cssText = 'animation: margin-bottom-pip---0 200ms;animation-fill-mode:forwards;transform: rotate(45deg);'
-    Variables.pip_ELs[1].style.opacity = '0'
-    Variables.pip_ELs[2].style.cssText = 'animation: margin-top-pip---0 200ms;animation-fill-mode:forwards;transform: rotate(-45deg);'
+let status_click_sidebarIcon = 'close'
+
+Variables.sidebarIcon_EL.addEventListener('click',event=>{
+    if (status_click_sidebarIcon==='close'){
+        Variables.sidebarIcon_EL.style.cssText = 'animation: open-sidebar-icon 300ms;animation-fill-mode: forwards;'
+        Variables.sidebarCont_EL.style.cssText = 'animation: open-sidebar-Cont 300ms;animation-fill-mode: forwards;'
+        status_click_sidebarIcon = 'open'
+    }else {
+        Variables.sidebarIcon_EL.style.cssText = 'animation: close-sidebar-icon 300ms;animation-fill-mode: forwards;'
+        Variables.sidebarCont_EL.style.cssText = 'animation: close-sidebar-Cont 300ms;animation-fill-mode: forwards;'
+        status_click_sidebarIcon = 'close'
+    }
 })
-
-
-
-
 
 
 
